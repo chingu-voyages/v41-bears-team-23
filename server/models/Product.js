072b71e1435ad49ce5ccfbd87a4394af336e8312
MJB <matt.berti13@gmail.com>
2022-10-20 18:08:16 -0500

    remove submodule attempt 1

---
+++ server/models/Product.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const ProductSchema = new Schema(
+    {
+        category: {
+            type: String,
+            required: true,
+          },
+          title: {
+            type: String,
+            required: true,
+          },
+          price:{
+            type: Integer,
+          },
+          description:{
+            type: String,
+            maxlength: 280,
+          },
+          image:{
+            type: Image,
+          },
+          rating:{
+            rate: Integer,
+            count: Integer,
+          }
+    }
+);
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String
+    email: String
+  },
+  type Product {
+    _id: ID!
+    name: String
+    
+  }
+  `
