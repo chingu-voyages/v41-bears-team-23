aad34ff5a1e6ce46775fbe36e0dc9b8b45efccbd
kavithachunduri <chunduri.kavitha@gmail.com>
2022-11-08 20:38:08 -0500

    connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://localhost:27017/blogDatabase'
+
 
 mongoose.connect(process.env.MONGODB_URI ||  MONGODB_URI, {
   useNewUrlParser: true,
