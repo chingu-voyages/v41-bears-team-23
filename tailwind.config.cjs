1e1c1a318c8b7d9f8bbee231a458b3b114dfcd9a
Angela Lee <angelawlee@rocketmail.com>
2022-11-08 21:04:55 -0600

    modify tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+        'login': "url('/src/assets/login.jpg')",
+      }
+		},
 	},
-	plugins: [require('daisyui')],
+	plugins: [],
 };
